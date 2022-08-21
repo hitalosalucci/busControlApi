f28b8f75ac41ea4a6c5853f75072ea3409faf5eb
Hitalo Salucci <hitalo.salucci@hotmail.com>
2022-08-21 07:42:27 -0300

    fix migration travels

--- database/migrations/2022_08_21_091811_create_travels_table.php
+++ database/migrations/2022_08_21_091811_create_travels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTravelTable extends Migration
+class CreateTravelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTravelTable extends Migration
      */
     public function up()
     {
-        Schema::create('travel', function (Blueprint $table) {
+        Schema::create('travels', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->index();
             $table->string('name', 200);
