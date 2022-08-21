f3fbadd1398ac57d57e1e68c72ba225d9fcb5002
Hitalo Salucci <hitalo.salucci@hotmail.com>
2022-08-21 16:17:39 -0300

    relacionamento cidade

--- app/Models/Destination.php
+++ app/Models/Destination.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\GeneratesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Destination extends Model
 {
-    use HasFactory;
+    use HasFactory, GeneratesUuid;
+
+    public function uuidColum()
+    {
+        return 'uuid';
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class, 'city_id', 'id');
+    }
 }
