e2a400800047148540dc79f1ff4568d5a8a453fa
Hitalo Salucci <hitalo.salucci@hotmail.com>
2022-08-21 16:18:14 -0300

    validadate password method

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class Driver extends Model
 {
     use HasFactory;
+
+    public function validatePassword(string $pass) : bool
+    {
+        return Hash::check($pass, $this->password);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,22 +2,33 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\GeneratesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, GeneratesUuid;
 
     protected $fillable = [
         'name',
         'email',
+        'login',
         'password',
     ];
 
     protected $hidden = [
         'password'
     ];
+
+    public function uuidColum()
+    {
+        return 'uuid';
+    }
+
+    public function validatePassword(string $pass) : bool
+    {
+        return Hash::check($pass, $this->password);
+    }
 }
