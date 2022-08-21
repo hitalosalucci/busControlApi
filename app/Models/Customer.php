238da488c8218325ec959d56eb95818214a77558
Hitalo Salucci <hitalo.salucci@hotmail.com>
2022-08-21 16:17:10 -0300

    Relacionamentos e validações e uuid colum

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\GeneratesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class Customer extends Model
 {
     use HasFactory;
+    use GeneratesUuid;
+
+    public function uuidColum()
+    {
+        return 'uuid';
+    }
+
+    public function validatePassword(string $pass) : bool
+    {
+        return Hash::check($pass, $this->password);
+    }
+
+    public function city()
+    {
+        return $this->hasOne(City::class, 'id', 'city_id');
+    }
 }
