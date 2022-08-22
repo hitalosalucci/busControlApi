150711cdf541e0319f0eb57a700f942c790437b2
Hitalo Salucci <hitalo.salucci@hotmail.com>
2022-08-22 05:41:31 -0300

    Relacionamento e testes

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -9,4 +9,22 @@ use Illuminate\Database\Eloquent\Model;
 class Travel extends Model
 {
     use HasFactory, GeneratesUuid;
+
+    protected $table = 'travels';
+    protected $primaryKey = 'id';
+
+    public function bus()
+    {
+        return $this->belongsTo(Bus::class, 'bus_id', 'id');
+    }
+
+    public function origin()
+    {
+        return $this->belongsTo(Origin::class, 'origin_id', 'id');
+    }
+
+    public function destination()
+    {
+        return $this->belongsTo(Destination::class, 'destination_id', 'id');
+    }
 }
--- tests/Unit/TravelUnitTest.php
+++ tests/Unit/TravelUnitTest.php
@@ -2,17 +2,35 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Travel;
+use App\Transactions\Travel\AddTravelTransaction;
+use DateTime;
+use Tests\TestCase;
 
 class TravelUnitTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    private $name = 'Viagem VIX X Cachoeiro de Itapemirim';
+    
+    public function testAddTravel()
     {
-        $this->assertTrue(true);
+        $date = '2022-04-23 12:30:19';
+        $bus = $this->createBus();
+        $origin = $this->createOrigin();
+        $destination = $this->createDestination();
+
+        $transaction = new AddTravelTransaction($this->name, $date, $origin->id, $destination->id, $bus->id);
+        $transaction->execute();
+
+        $travel = Travel::first();
+
+        $this->assertNotNull($travel, 'Viagem inválido');
+        $this->assertNotNull($travel->uuid, 'UUID inválido');
+        $this->assertEquals($this->name, $travel->name, 'Nome incorreto');
+        $this->assertEquals($date, $travel->date, 'Data incorreta');
+        $this->assertEquals($bus->id, $travel->bus->id, 'Ônibus incorreto');
+        $this->assertEquals($origin->id, $travel->origin->id, 'Origem incorreta');
+        $this->assertEquals($destination->id, $travel->destination->id, 'Destino incorreto');
+
     }
+
 }
