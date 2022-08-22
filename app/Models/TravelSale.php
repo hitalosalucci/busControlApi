d0a81eb41318c9963f7b1a6557ebf3ce122b5bfb
Hitalo Salucci <hitalo.salucci@hotmail.com>
2022-08-22 09:41:28 -0300

    method checkBusChairUsed

--- app/Models/TravelSale.php
+++ app/Models/TravelSale.php
@@ -15,6 +15,11 @@ class TravelSale extends Model
         return $this->belongsTo(Bus::class, 'bus_id', 'id');
     }
 
+    public function busChair()
+    {
+        return $this->belongsTo(BusChair::class, 'bus_chair_id', 'id');
+    }
+
     public function customer()
     {
         return $this->belongsTo(Customer::class, 'customer_id', 'id');
@@ -28,5 +33,15 @@ class TravelSale extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function checkBusChairUsed($busChairIdCheck)
+    {
+        $busChair = $this->where('bus_chair_id', $busChairIdCheck)->first();
+
+        if (is_null($busChair))
+            return false;
+         
+        return true;
     }
 }
