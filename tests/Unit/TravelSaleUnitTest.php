07385476324a9715cc8c6a48232eb20e0e29b28c
Hitalo Salucci <hitalo.salucci@hotmail.com>
2022-08-22 21:11:56 -0300

    testeUpdateTravelSale em andamento

--- tests/Unit/TravelSaleUnitTest.php
+++ tests/Unit/TravelSaleUnitTest.php
@@ -57,4 +57,28 @@ class TravelSaleUnitTest extends TestCase
         $this->assertEquals($busChair->id, $travelSale->busChair->id, 'Poltrona do ônibus incorreto');
     }
 
+    // public function testUpdateTravelSale()
+    // {
+    //     $busChair = $this->createBusChair();
+    //     $bus = $busChair->bus;
+    //     $travel = $this->createTravel();
+    //     $customer = $this->createCustomer();
+    //     $user = $this->createUser();
+
+    //     $transaction = new AddTravelSaleTransaction($this->price, $travel->id, $busChair->id, $bus->id, $customer->id, $user->id);
+    //     $transaction->execute();
+        
+    //     $travelSale = TravelSale::first();
+        
+    //     $this->assertNotNull($travelSale, 'Venda viagem inválida');
+    //     $this->assertNotNull($travelSale->uuid, 'UUID inválido');
+    //     $this->assertEquals($this->price, $travelSale->price, 'Preco incorreto');
+    //     $this->assertEquals($bus->id, $travelSale->bus->id, 'Preco incorreto');
+    //     $this->assertEquals($bus->id, $travelSale->bus->id, 'Ônibus incorreto');
+    //     $this->assertEquals($travel->id, $travelSale->travel->id, 'Viagem incorreta');
+    //     $this->assertEquals($customer->id, $travelSale->customer->id, 'Cliente incorreto');
+    //     $this->assertEquals($user->id, $travelSale->user->id, 'Usuário incorreto');
+    //     $this->assertEquals($busChair->id, $travelSale->busChair->id, 'Poltrona do ônibus incorreto');
+    // }
+
 }
