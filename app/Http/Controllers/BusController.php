70a2ad0837ccdcbd7fa6a1fb9f9a5b30aaf4924a
Hitalo Salucci <hitalo.salucci@hotmail.com>
2022-08-22 21:12:24 -0300

    get all Buses

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bus;
 use Illuminate\Http\Request;
 
 class BusController extends Controller
 {
-    //
+    public function getAllBuses()
+    {
+        $buses = Bus::all();
+
+        $busArray = [];
+        foreach ($buses as $bus)
+        {
+            $busArray[] = [
+                'uuid' => $bus->uuid,
+                'description' => $bus->description,
+                'model' => $bus->model,
+                'manufacturer' => $bus->manufacturer,
+                'created_at' => $bus->created_at,
+            ];
+        }
+
+        $returnArray = [
+            'status' => 200,
+            'error' => false,
+            'data' => $busArray
+        ];
+
+        return $returnArray;
+    }
 }
